230e45b0f3573712035fda6b25220857bbe57be8
Andrew White <andrewh@cdw.com>
2026-07-27 07:27:44 -0500

    fix: add missing import for dream.utilities module

--- scripts/analyze_training_multi.py
+++ scripts/analyze_training_multi.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import dream.analysis as dream_analysis
+import dream.utilities
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
